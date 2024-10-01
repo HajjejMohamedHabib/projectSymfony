77ced0a98dfaf1eb8cad3e931554178ed7216564
mohamed-hajjej <56257070+mohamed-hajjej@users.noreply.github.com>
2024-10-01 19:25:47 +0100

    faire de selections de la base de données via les methodes findall et find de Repository

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -7,28 +7,40 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/personne')]
 class PersonneController extends AbstractController
 {
-    #[Route('/addpersonne', name: 'add_personne')]
-    public function index(ManagerRegistry $doctrine): Response
+    #[Route('/liste', name: 'personne.liste')]
+    public function index(ManagerRegistry $doctrine): Response{
+        $repository = $doctrine->getRepository(Personne::class);
+        $personnes = $repository->findAll();
+        return $this->render('personne/index.html.twig',['personnes'=>$personnes]);
+    }
+    #[Route('/detail/{id<\d+>}', name: 'personne.detail')]
+    public function detailPersonne (ManagerRegistry $doctrine, $id): Response
+    {
+        $repository = $doctrine->getRepository(Personne::class);
+        $personne = $repository->find($id);
+        if(!$personne){
+            $this->addFlash('error',"Personne n'existe pas");
+            return $this->redirectToRoute('personne.liste');
+        }
+        return $this->render('personne/detail.html.twig',['personne'=>$personne]);
+    }
+    #[Route('/add', name: 'personne.add')]
+    public function addPersonne(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
-        $personne1=new Personne();
-        $personne1->setFirstname('mohamed');
-        $personne1->setLastname('hajjej');
-        $personne1->setAge(28);
-        $personne2=new Personne();
-        $personne2->setFirstname('ahmed');
-        $personne2->setLastname('hajjej');
-        $personne2->setAge(30);
+        $personne=new Personne();
+        $personne->setFirstname('mohamed');
+        $personne->setLastname('hajjej');
+        $personne->setAge(28);
         // ajouter l'operation d'insertion de personne a la transaction
-        $entityManager->persist($personne1);
-        $entityManager->persist($personne2);
+        $entityManager->persist($personne);
         // execution de la transaction
         $entityManager->flush();
         return $this->render('personne/detail.html.twig', [
-         'personne1' => $personne1,'personne2' => $personne2
+         'personne' => $personne
         ]);
     }
 }
